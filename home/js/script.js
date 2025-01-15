a9af0b1bfa513c5482962fe3328a11ba27b76c20
Feliz Salting <fpsalting@student.apc.edu.ph>
2025-01-16 07:53:38 +0800

    flexbox att

---
+++ home/js/script.js
@@ -0,0 +1,9 @@
+function showContent(id) 
+    const contents = document.querySelectorAll('.folder-content');
+    contents.forEach(content => content.classList.remove('active'));
+    const content = document.getElementById(id);
+    if (content) {
+      content.classList.add('active');
+    }
+  }
+  
