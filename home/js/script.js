70af6a210af73ff671ec9aff75416f98a4b2196a
Feliz Salting <fpsalting@student.apc.edu.ph>
2025-01-16 08:20:09 +0800

    att

--- home/js/script.js
+++ home/js/script.js
@@ -1,9 +1,9 @@
-function showContent(id) 
+function showContent(id) {
     const contents = document.querySelectorAll('.folder-content');
     contents.forEach(content => content.classList.remove('active'));
+
     const content = document.getElementById(id);
     if (content) {
-      content.classList.add('active');
+        content.classList.add('active');
     }
-  }
-  
+}
